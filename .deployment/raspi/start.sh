bec18ed6c156dc89dff04ce4704734722de50158
Pat Jacobs <musicbender@gmail.com>
2022-09-25 11:04:16 -0700

    update db config

--- .deployment/raspi/start.sh
+++ .deployment/raspi/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set environment variables
-source ~/apps/gaspar/scripts/setenv.sh
+source ~/apps/gaspar/setenv.sh
 
 echo testing env vars...
 echo $GASPAR_AWS_REGION
@@ -10,4 +10,4 @@ echo $GASPAR_AWS_REGION
 aws ecr get-login-password | docker login --username AWS --password-stdin ${GASPAR_AWS_ACCOUNT_ID}.dkr.ecr.${GASPAR_AWS_REGION}.amazonaws.com
 
 # run gaspar
-docker-compose -f docker-compose.prod.yml up
+docker-compose -f docker-compose.prod.yml up --build 
