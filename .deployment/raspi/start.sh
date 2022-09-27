868cedb2ca5f967876872f687b4dfc2c5e5b0d88
Pat Jacobs <musicbender@gmail.com>
2022-09-26 19:54:38 -0700

    arm64 support

--- .deployment/raspi/start.sh
+++ .deployment/raspi/start.sh
@@ -3,11 +3,8 @@
 # set environment variables
 source ~/apps/gaspar/setenv.sh
 
-echo testing env vars...
-echo $GASPAR_AWS_REGION
-
 # login to ECR
 aws ecr get-login-password | docker login --username AWS --password-stdin ${GASPAR_AWS_ACCOUNT_ID}.dkr.ecr.${GASPAR_AWS_REGION}.amazonaws.com
 
 # run gaspar
-docker-compose -f docker-compose.prod.yml up --build 
+docker-compose -f docker-compose.prod.yml up 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 rsync -av ~/my-projects/gaspar/.deployment/raspi/ pi@192.168.86.105:~/apps/gaspar
