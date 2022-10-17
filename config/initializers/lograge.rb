1550f197118e5f81cc1f419a1af8fc5f5355ef52
Pat Jacobs <musicbender@gmail.com>
2022-10-16 22:20:12 -0700

    fix logging

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.base_controller_class = 'ActionController::API'
+  config.lograge.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new($stdout))
 end
